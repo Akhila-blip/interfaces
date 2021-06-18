b16dbf591960ac0349743f1715f4a424e12eb097
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-18 18:02:17 -0500

    Added FunctionalExample program

---
+++ FunctionalExample.java
@@ -0,0 +1,17 @@
+class FunctionalExample
+{
+
+public static void main(String[] arg)
+
+{
+
+FuntionalInterfaceEx fn=(int i, int j) ->
+ {
+System.out.println("ram"+(i+j));
+	
+};
+fn.sum(10,20);
+}
+}
+
+
